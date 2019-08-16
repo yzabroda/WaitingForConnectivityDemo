acf16824f1a2459f8a3444d3fe5e3ca20177fcf4
Yuriy Zabroda <Yuriy.Zabroda@ua.ibm.com>
2019-08-16 12:44:04 +0300

    Initial Commit

---
+++ WaitingForConnectivityDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaitingForConnectivityDemo
+//
+//  Created by Yuriy Zabroda on 8/16/19.
+//  Copyright © 2019 Yuriy Zabroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
